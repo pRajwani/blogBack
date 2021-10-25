8610b585bbac7400f5e232074df1deadf40a6ab7
puneet rajwani <puneetrajwani@gmail.com>
2021-10-25 10:39:03 +0530

    added cors

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('express-async-errors');
 require('./db');
+const cors = require('cors');
 
 const errorhandler = require('./util/errorHandler');
 var blogRouter = require('./routes/blog');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
